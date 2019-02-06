4a0bf2eeb7a7b5a0cabdba6d3a6101e36d165078
Vincent_Boutot <VincentBoutot@live.ca>
2019-02-06 16:24:23 -0500

    ajout des cas d'erreur au programme

--- src/Main.java
+++ src/Main.java
@@ -46,7 +46,7 @@ public class Main {
 			System.out.println("");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("Les entrées du fichier ne sont pas conformes!");
 		};
 		
 		System.out.println("Bienvenue chez Barette!\r\n" + 
